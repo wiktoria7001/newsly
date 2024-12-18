4984f7eacd88a13399de92f4b7ece57ca48bd048
wiktoria7001 <93009130+wiktoria7001@users.noreply.github.com>
2024-12-18 19:44:12 +0100

    landing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsly/pages/home.dart';
+import 'package:newsly/pages/landing_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Home()
+      home: LandingPage()
     );
   }
 }
